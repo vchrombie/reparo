7813401dd224e220aa9a75a68084e98b22d95fec
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2020-03-08 02:33:53 +0530

    Add script and template
    Signed-off-by: Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>

---
+++ script.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+from string import Template
+
+REPO = "chaoss/grimoirelab-perceval/"
+PATH_TO_FILE = "perceval/backend.py"
+
+data = requests.get("https://api.github.com/repos/"+REPO+"commits?path="+PATH_TO_FILE)
+data = json.loads(data.content)
+
+authors_data = {}
+
+for item in data:
+    data = item['commit']['author']
+    if data['name'] not in authors_data.keys():
+        authors_data[data['name']] = data['email']
+
+authors = [key+" <"+value+">" for key, value in authors_data.items()]
+
+# print(authors)
+
+template_file = open("gpl-v3.tmpl")
+
+src = Template(template_file.read())
+
+years = "2015-2020"
+owner = "Bitergia"
+
+sub_dict = {
+    'years':years,
+    'owner':owner,
+    'authors':'\n#     '.join(authors)
+}
+
+result = src.substitute(sub_dict)
+
+print(result)
