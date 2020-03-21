e858852848203438b4ccb5f11b6e2d3bc222e9d8
Sanjana Nayar <sanjananayar2k1@gmail.com>
2020-03-21 20:16:50 +0530

    Enable support to run it in repos
    Signed-off-by: Sanjana Nayar <sanjananayar2k1@gmail.com>

--- reparo.py
+++ reparo.py
@@ -22,57 +22,95 @@
 
 import requests
 import json
-
+import os
 from string import Template
-
-REPO = "chaoss/grimoirelab-perceval/"
-PATH_TO_FILE = "perceval/backend.py"
-
-data = requests.get("https://api.github.com/repos/" + REPO + "commits?path=" + PATH_TO_FILE)
-data = json.loads(data.content)
-
-authors_data = {}
-
-for item in reversed(data):
-    data = item['commit']['author']
-    if data['name'] not in authors_data.keys():
-        authors_data[data['name']] = data['email']
-
-authors = [key + " <" + value + ">" for key, value in authors_data.items()]
-
-# print(authors)
-
-template_file = open("gpl-v3.tmpl")
-
-src = Template(template_file.read())
 
 years = "2015-2020"
 owner = "Bitergia"
 
-sub_dict = {
-    'years': years,
-    'owner': owner,
-    'authors': '\n#     '.join(authors)
-}
 
-result = src.substitute(sub_dict)
+def getlistoffiles(dirname):
+    listoffile = os.listdir(dirname)
+    allfiles = list()
+    for entry in listoffile:
+        fullpath = os.path.join(dirname, entry)
+        if os.path.isdir(fullpath):
+            allfiles = allfiles+getlistoffiles(fullpath)
+        else:
+            allfiles.append(fullpath)
+    return(allfiles)
 
-# print(result)
 
-with open('./backend.py', 'r') as f:
-    contents = f.readlines()
-    i = 0
-    for item in contents:
-        if item.startswith('#'):
-            i += 1
+REPO = input("repo name: ")
+dirname = input("path to repo: ")
 
-with open('./backend.py', 'w') as f:
-    f.writelines(contents[i:])
+listoffiles = getlistoffiles(dirname)
 
-with open('./backend.py', 'r') as f:
-    contents = f.readlines()
-    contents.insert(0, result+"\n")
+FILES = []
 
-with open('./backend.py', 'w') as f:
-    contents = "".join(contents)
-    f.write(contents)
+for f in listoffiles:
+    count = 0
+    try:
+        for line in open(f):
+            if "Copyright (C)" and "Authors" in line:
+                count = count+1
+        if(count > 0):
+            FILES.append(f)
+    except UnicodeDecodeError:
+        pass
+
+
+for items2 in FILES:
+
+    PATH_TO_FILE2 = items2.replace(dirname, '')
+    PATH_TO_FILE = PATH_TO_FILE2.strip('/')
+    CHAOSS_HTTP = "https://api.github.com/repos/chaoss/"
+
+    data = requests.get(CHAOSS_HTTP + REPO + "/commits?path=" + PATH_TO_FILE)
+
+    data = json.loads(data.content)
+
+    authors_data = {}
+
+    try:
+        for item in reversed(data):
+            data = item['commit']['author']
+            if data['name'] not in authors_data.keys():
+                authors_data[data['name']] = data['email']
+    except TypeError:
+        pass
+
+    authors = [key + " <" + value + ">" for key, value in authors_data.items()]
+    authors.append('')
+
+    # print(authors)
+
+    template_file = open("gpl-v3.tmpl")
+
+    src = Template(template_file.read())
+
+    sub_dict = {
+        'years': years,
+        'owner': owner,
+        'authors': '\n#     '.join(authors)
+    }
+
+    result = src.substitute(sub_dict)
+
+    with open(items2, 'r') as f:
+        contents = f.readlines()
+        i = 0
+        for item in contents:
+            if item.startswith('#'):
+                i += 1
+
+    with open(items2, 'w') as f:
+        f.writelines(contents[i:])
+
+    with open(items2, 'r') as f:
+        contents = f.readlines()
+        contents.insert(0, result+"\n")
+
+    with open(items2, 'w') as f:
+        contents = "".join(contents)
+        f.write(contents)
